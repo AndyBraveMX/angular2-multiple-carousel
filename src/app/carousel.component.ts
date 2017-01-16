f3a051e77765d938c50d1cedf904345ec77783b2
Andy Bravo <anbrav@hotmail.com>
2017-01-16 14:37:09 -0600

    Se segregan los archivos para estilos y html

--- src/app/carousel.component.ts
+++ src/app/carousel.component.ts
@@ -15,87 +15,8 @@ import {   trigger,
   //Name of our tag
   selector: 'css-carousel',
   animations: [],
-  //Template for the tag
-  template: `
-  <div class="carousel">
-  	<ul class="slides">
-  		<li *ngFor="let image of images">
-  			<h2>{{image.title}}</h2>
-  			<img src="{{image.url}}" alt="">
-  		</li>
-  	</ul>
-  	<a class="left carousel-control" (click)="prev()" *ngIf="images.length" role="button" data-slide="prev">
-  		<span class="icon-prev" aria-hidden="true"></span>
-  		<span *ngIf="isBS4" class="sr-only">Previous</span>
-  	</a>
-  	<a class="right carousel-control" (click)="next()" *ngIf="images.length" role="button" data-slide="next">
-  		<span class="icon-next" aria-hidden="true"></span>
-  		<span *ngIf="isBS4" class="sr-only">Next</span>
-  	</a>
-  </div>
-  `,
-  //Styles for the tag
-  styles: [`
-.carousel{
-    overflow:hidden;
-    width:100%;
-}
-.slides{
-    list-style:none;
-    position:relative;
-    width:800%; /* Number of panes * 100% */
-    overflow:hidden; /* Clear floats */
-        /* Slide effect Animations*/
-    -moz-animation:carousel 30s infinite;
-    -webkit-animation:carousel 30s infinite;
-    animation:carousel 30s infinite;
-}
-.slides > li{
-     position:relative;
-     float:left;
-     width: 3%; /* 100 / number of panes */
-     display: inline-block;
-     margin-bottom: 8px;
-     margin-right: 8px;
-}
-.carousel img{
-    display:block;
-    width:100%;
-    max-width:100%;
-}
-.carousel h2{
-    margin-bottom: 0;
-    font-size:1em;
-    padding:1.5em 0.5em 1.5em 0.5em;
-    position:absolute;
-    right:0px;
-    bottom:0px;
-    left:0px;
-    text-align:center;
-    color:#fff;
-    background-color:rgba(0,0,0,0.75);
-    text-transform: uppercase;
-}
-@keyframes carousel{
-    0%    { left:-5%; }
-    11%   { left:-5%; }
-    12.5% { left:-105%; }
-    23.5% { left:-105%; }
-    25%   { left:-205%; }
-    36%   { left:-205%; }
-    37.5% { left:-305%; }
-    48.5% { left:-305%; }
-    50%   { left:-405%; }
-    61%   { left:-405%; }
-    62.5% { left:-305%; }
-    73.5% { left:-305%; }
-    75%   { left:-205%; }
-    86%   { left:-205%; }
-    87.5% { left:-105%; }
-    98.5% { left:-105%; }
-    100%  { left:-5%; }
-}
-  `]
+  templateUrl: './carousel.component.html',
+  styleUrls: [ './carousel.component.css' ]
 })
 //Carousel Component itself
 export class CSSCarouselComponent {
